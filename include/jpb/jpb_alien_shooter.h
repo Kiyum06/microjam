fba45f0537315838235a520f06fc6bcafb96fe78
JPbooya <perezjcarlos81@gmail.com>
2026-03-05 19:41:57 +0000

    Added game header file

---
+++ include/jpb/jpb_alien_shooter.h
@@ -0,0 +1,20 @@
+#include "mj/mj_game.h"
+
+namespace jpb {
+  class jpb_alien_shooter : public mj::game {
+    public: 
+      jpb_alien_shooter (int completed_games, const mj::game_data& data);
+
+      bn::string<16> title() const override;
+
+      int total_frames() const override;
+
+      mj::game_result play(const mj::game_data& data)override;
+
+      bool victory() const override;
+
+      void fade_in(const mj::game_data& data)override;
+
+      void fade_out(const mj::game_data& data)override;
+  }
+}
